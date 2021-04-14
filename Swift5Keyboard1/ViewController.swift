bcd925f3949f52cc410c0b69fd3eb5311c0feb45
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-04-15 00:50:25 +0900

    入力フォームの内容をlabelに反映

--- Swift5Keyboard1/ViewController.swift
+++ Swift5Keyboard1/ViewController.swift
@@ -15,15 +15,24 @@ class ViewController: UIViewController {
         
     @IBOutlet weak var passWordTextField: UITextField!
     
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var passWordLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
 
     
     @IBAction func login(_ sender: Any) {
+        
+        userNameLabel.text = userNameTextField.text
+        
+        passWordLabel.text = passWordTextField.text
+        
     }
     
 }
